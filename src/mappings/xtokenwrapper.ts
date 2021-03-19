559f3aad6fcf9841ce268351856d42833a9dc852
Gabriel Nicolás González <gabriel@camba.coop>
2021-03-19 14:42:19 -0300

    Added token and xtoken metadata and started consuming new deployment on kovan

--- src/mappings/xtokenwrapper.ts
+++ src/mappings/xtokenwrapper.ts
@@ -1,6 +1,9 @@
-import { Address } from '@graphprotocol/graph-ts'
 import { RegisterToken } from '../types/XTokenWrapper/XTokenWrapper'
 import { Token, XToken } from '../types/schema'
+import { ERC20 } from '../types/XTokenWrapper/ERC20'
+import { Address } from '@graphprotocol/graph-ts'
+
+const DEFAULT_DECIMALS = 18
 
 export function handleRegisterToken(event: RegisterToken): void {   
     let tokenId = event.params.token.toHex()
@@ -11,13 +14,33 @@ export function handleRegisterToken(event: RegisterToken): void {
 
     if (token == null) {
         token = new Token(tokenId)
+        let erc20Token = ERC20.bind(event.params.token)
+        let tokenDecimals = erc20Token.try_decimals()
+        let tokenName = erc20Token.try_name()
+        let tokenSymbol = erc20Token.try_symbol()
+        token.decimals = !tokenDecimals.reverted
+            ? tokenDecimals.value
+            : DEFAULT_DECIMALS
+        token.name = !tokenName.reverted ? tokenName.value : ""
+        token.symbol = !tokenSymbol.reverted ? tokenSymbol.value : ""
     }
+
     if (xToken == null) {
         xToken = new XToken(xTokenId)
+        let erc20Token = ERC20.bind(event.params.xToken)
+        let tokenDecimals = erc20Token.try_decimals()
+        let tokenName = erc20Token.try_name()
+        let tokenSymbol = erc20Token.try_symbol()
+        xToken.decimals = !tokenDecimals.reverted
+            ? tokenDecimals.value
+            : DEFAULT_DECIMALS
+        xToken.name = !tokenName.reverted ? tokenName.value : ""
+        xToken.symbol = !tokenSymbol.reverted ? tokenSymbol.value : ""
     }
+
     xToken.token = tokenId
 
     token.save()
     xToken.save()
-
 }
+
