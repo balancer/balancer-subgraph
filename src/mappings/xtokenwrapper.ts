eacfaa10f1c5de095caf1b9a4164d9055274a5d7
Gabriel Nicolás González <gabriel@camba.coop>
2021-03-25 16:28:47 -0300

    Handling paused un paused for xTokens, and switched to last version deployed

--- src/mappings/xtokenwrapper.ts
+++ src/mappings/xtokenwrapper.ts
@@ -1,20 +1,24 @@
 import { RegisterToken } from '../types/XTokenWrapper/XTokenWrapper'
 import { Token, XToken } from '../types/schema'
 import { ERC20 } from '../types/XTokenWrapper/ERC20'
+import { Paused, Unpaused } from '../types/templates/XToken/XToken'
+import { XToken as XTokenAbi } from '../types/templates'
 import { Address } from '@graphprotocol/graph-ts'
 
 const DEFAULT_DECIMALS = 18
 
 export function handleRegisterToken(event: RegisterToken): void {   
-    let tokenId = event.params.token.toHex()
-    let xTokenId = event.params.xToken.toHex()
+    let tokenIdAddress = event.params.token
+    let xTokenIdAddress = event.params.xToken
+    let tokenId = tokenIdAddress.toHex()
+    let xTokenId = xTokenIdAddress.toHex()
 
     let token = Token.load(tokenId)
     let xToken = XToken.load(xTokenId)
 
     if (token == null) {
         token = new Token(tokenId)
-        let erc20Token = ERC20.bind(event.params.token)
+        let erc20Token = ERC20.bind(tokenIdAddress)
         let tokenDecimals = erc20Token.try_decimals()
         let tokenName = erc20Token.try_name()
         let tokenSymbol = erc20Token.try_symbol()
@@ -27,7 +31,7 @@ export function handleRegisterToken(event: RegisterToken): void {
 
     if (xToken == null) {
         xToken = new XToken(xTokenId)
-        let erc20Token = ERC20.bind(event.params.xToken)
+        let erc20Token = ERC20.bind(xTokenIdAddress)
         let tokenDecimals = erc20Token.try_decimals()
         let tokenName = erc20Token.try_name()
         let tokenSymbol = erc20Token.try_symbol()
@@ -36,11 +40,28 @@ export function handleRegisterToken(event: RegisterToken): void {
             : DEFAULT_DECIMALS
         xToken.name = !tokenName.reverted ? tokenName.value : ""
         xToken.symbol = !tokenSymbol.reverted ? tokenSymbol.value : ""
+        xToken.paused = false
     }
 
     xToken.token = tokenId
+    XTokenAbi.create(xTokenIdAddress)
 
     token.save()
     xToken.save()
 }
 
+export function handlePaused(event: Paused): void {
+    changeXTokenState(event.address, true)
+}
+
+export function handleUnpaused(event: Unpaused): void {
+    changeXTokenState(event.address, false)
+}
+
+function changeXTokenState(xTokenAddress: Address, xTokenState: boolean): void{
+    let xTokenId = xTokenAddress.toHexString()
+    let xToken = XToken.load(xTokenId)
+
+    xToken.paused = xTokenState
+    xToken.save()
+}
