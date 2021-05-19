e12b65ae33444fe53e53a04e8dbcc166f4781722
Gabriel Nicolás González <gabriel@camba.coop>
2021-05-19 16:07:47 -0300

    indexing BPoolProxy and adding deploy:rinkeby option

---
+++ src/mappings/bpoolproxy.ts
@@ -0,0 +1,51 @@
+import {
+    JoinPool,
+    ExitPool
+} from '../types/BPoolProxy/BPoolProxy'
+
+import {
+    PoolShare,
+} from '../types/schema'
+
+import {
+    createPoolShareEntity, 
+    ZERO_BD 
+} from './helpers'
+
+export function handleJoinPool(event: JoinPool): void {
+    let bpool = event.params.bpool
+    let lp = event.params.liquidityProvider
+    let shares = event.params.shares
+
+    let bpoolId = bpool.toHex()
+    let lpId = lp.toHex()
+    let poolShareId = bpoolId.concat('-').concat(lpId)
+
+    let poolShare = PoolShare.load(poolShareId)
+    
+    let poolShareBalance = poolShare == null ? ZERO_BD : poolShare.balance
+
+    if (poolShare == null) {
+        createPoolShareEntity(poolShareId, bpoolId, lpId)
+        poolShare = PoolShare.load(poolShareId)
+    }
+    
+    poolShare.balance = poolShareBalance.plus(shares.toBigDecimal())
+    poolShare.save()
+}
+
+export function handleExitPool(event: ExitPool): void {
+    let bpool = event.params.bpool
+    let lp = event.params.iquidityProvider
+    let shares = event.params.shares
+
+    let bpoolId = bpool.toHex()
+    let lpId = lp.toHex()
+    let poolShareId = bpoolId.concat('-').concat(lpId)
+
+    let poolShare = PoolShare.load(poolShareId)
+    let poolShareBalance = poolShare.balance
+
+    poolShare.balance = poolShareBalance.minus(shares.toBigDecimal())
+    poolShare.save()
+}
